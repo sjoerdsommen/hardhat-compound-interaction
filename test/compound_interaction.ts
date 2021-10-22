2879cc9a8393d865b331cb37bf2af67daa48b3c8
sjoerdsommen <sjoerdsommen@gmail.com>
2021-10-22 16:58:28 +0200

    started writing the compound interaction tests

---
+++ test/compound_interaction.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Compound Interaction", function () {
+    it("Balance of owner in cdai contract should be > 0", async function () {
+        
+    });
+});
