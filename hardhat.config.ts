8503675461b1c83db47b5e167e0d24fc99d7330b
sjoerdsommen <sjoerdsommen@gmail.com>
2021-10-24 11:45:36 +0200

    tests work

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,7 +33,7 @@ const config: HardhatUserConfig = {
     hardhat: {
       forking: {
         url: process.env.WEB3_INFURA_PROJECT_ID || "",
-        blockNumber: 13475333,
+        // blockNumber: 13475333,
       },
     },
   },
--- test/compound_interaction.ts
+++ test/compound_interaction.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import * as dai from "../abis/dai.json";
+import dai from "../abis/dai.json";
 
 describe("Test unlocking account", function () {
   it("Balance of dai should be > 0", async function () {
@@ -9,6 +9,19 @@ describe("Test unlocking account", function () {
     const signer = await ethers.getSigner(acc);
     const daiContract = new ethers.Contract(daiAddress, dai, signer);
     const daiBalance = await daiContract.balanceOf(acc);
-    expect(daiBalance).to.be.greaterThan(0);
+    const formattedBalace = parseFloat(ethers.utils.formatEther(daiBalance));
+    expect(formattedBalace).to.be.greaterThan(0);
+  });
+});
+
+describe("Test Compound Interaction", function () {
+  it("Balance of cdai should be > 0", async function () {
+    const acc = "0x21a31Ee1afC51d94C2eFcCAa2092aD1028285549";
+    const daiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const signer = await ethers.getSigner(acc);
+    const daiContract = new ethers.Contract(daiAddress, dai, signer);
+    const daiBalance = await daiContract.balanceOf(acc);
+    const formattedBalace = parseFloat(ethers.utils.formatEther(daiBalance));
+    expect(formattedBalace).to.be.greaterThan(0);
   });
 });
