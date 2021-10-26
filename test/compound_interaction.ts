e92f018c003cec1464d1dc7058f6977145bf1393
sjoerdsommen <sjoerdsommen@gmail.com>
2021-10-26 14:12:26 +0200

    able to get some dai

--- test/compound_interaction.ts
+++ test/compound_interaction.ts
@@ -1,21 +1,23 @@
 import { expect } from "chai";
-import { ethers } from "hardhat";
-// import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { ethers, network } from "hardhat";
 import dai from "../abis/dai.json";
-// import cdai from "../abis/cdai.json";
 
 describe("Testing unlocking DAI and trying to send it", function () {
-  const acc = "0x21a31Ee1afC51d94C2eFcCAa2092aD1028285549";
+  const acc = "0x21a31Ee1afC51d94C2eFcCAa2092aD1028285549"; // whale account
   const daiAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 
   it("Balance of dai should be > 0", async function () {
+    await network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [acc],
+    });
     const signer = await ethers.getSigner(acc);
     const [ownAcc] = await ethers.getSigners();
     const daiContract = new ethers.Contract(daiAddress, dai);
-    const daiBalanceSigner = await daiContract.balanceOf(signer);
-    console.log(daiBalanceSigner);
-    await daiContract.connect(signer).transfer(ownAcc, 1);
-    const daiBalance = await daiContract.balanceOf(ownAcc);
+    await daiContract.connect(signer).transfer(ownAcc.address, 1);
+    const daiBalance = await daiContract
+      .connect(signer)
+      .balanceOf(ownAcc.address);
     const formattedBalance = parseFloat(ethers.utils.formatEther(daiBalance));
     expect(formattedBalance).to.be.greaterThan(0);
   });
